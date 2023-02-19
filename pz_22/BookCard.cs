5444a91b5eb05db906638c683e712362f49d7724
Золин Никита <zolinnikita69@gmail.com>
2023-02-19 17:53:13 +0500

    Добавьте файлы проекта.

---
+++ pz_22/BookCard.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pz_22
+{
+    internal class BookCard
+    {
+        enum Condition
+        {
+            ToWriteOff,
+            Good,
+            New
+        }
+
+        class BookCard
+        {
+            private int ID;
+            private string author;
+            private string title;
+            private int year;
+            private Condition condition;
+
+            private static int booksToWriteOffCount;
+            private static int goodBooksCount;
+            private static int newBooksCount;
+
+            public BookCard(int ID, string author, string title, int year, Condition condition)
+            {
+                this.ID = ID;
+                this.author = !string.IsNullOrEmpty(author) ? author : throw new ArgumentException("Author name cannot be null or empty.");
+                this.title = !string.IsNullOrEmpty(title) ? title : throw new ArgumentException("Title cannot be null or empty.");
+                this.year = year >= 1990 ? year : throw new ArgumentOutOfRangeException("Year of publication cannot be earlier than 1990.");
+                this.condition = condition;
+
+                switch (condition)
+                {
+                    case Condition.ToWriteOff:
+                        booksToWriteOffCount++;
+                        break;
+                    case Condition.Good:
+                        goodBooksCount++;
+                        break;
+                    case Condition.New:
+                        newBooksCount++;
+                        break;
+                }
+            }
+
+            public void GetBookInfo()
+            {
+                Console.WriteLine("ID: {0}\nAuthor: {1}\nTitle: {2}\nYear: {3}\nCondition: {4}", ID, author, title, year, condition);
+            }
+
+            public static void GetBookStats()
+            {
+                Console.WriteLine("Books to write off: {0}\nGood books: {1}\nNew books: {2}", booksToWriteOffCount, goodBooksCount, newBooksCount);
+            }
+        }
+    }
+}
---
+++ pz_22/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+using pz_22;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        BookCard book1 = new BookCard(1, "Jane Austen", "Pride and Prejudice", 1813, Condition.Good);
+        BookCard book2 = new BookCard(2, "Fyodor Dostoevsky", "Crime and Punishment", 1866, Condition.ToWriteOff);
+        BookCard book3 = new BookCard(3, "Harper Lee", "To Kill a Mockingbird", 1960, Condition.New);
+
+        book1.GetBookInfo();
+        Console.WriteLine();
+        book2.GetBookInfo();
+        Console.WriteLine();
+        book3.GetBookInfo();
+        Console.WriteLine();
+
+        BookCard.GetBookStats();
+    }
+}
+
